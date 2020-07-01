66c3fd3aa018f01290c4f87cb3a5a47162aa498e
Adrien Saremi <adriensaremi@gmail.com>
2020-06-30 20:19:31 -0400

    mini-project update and ultimate challenge

---
+++ Exercises/Ex_Files_TensorFlow/Exercise Files/02/addition.py
@@ -0,0 +1,19 @@
+import os
+import tensorflow as tf
+
+# Turn off TensorFlow warning messages in program output
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Define computational graph
+X =
+Y =
+
+addition =
+
+
+# Create the session
+with tf.Session() as session:
+
+    result =
+
+    print(result)
